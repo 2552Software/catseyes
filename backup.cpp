137e63d0a9a58ee94ccf1c3507951f16c57efd92
Mark S <mark.shavlik@shavlik.us>
2018-10-15 11:07:39 -0500

    Update backup.cpp

--- backup.cpp
+++ backup.cpp
@@ -38,11 +38,75 @@ private:
 };
 
 
-//--------------------------------------------------------------
 #include "ofApp.h"
+
+void ofxAnimatableQueue2552::setInitialValue(const ofxAnimatableOfPoint val) {
+    anim = val;
+}
+
+
+void ofxAnimatableQueue2552::addTransition(const ofxAnimatableOfPoint targetValue) {
+    animSteps.push_back(targetValue);
+}
+
+
+void ofxAnimatableQueue2552::clearQueue() {
+    animSteps.clear();
+}
+
+
+void ofxAnimatableQueue2552::update(float dt) {
+
+    if (playing) {
+
+        anim.update(dt);
+
+        if (anim.hasFinishedAnimating()) {
+            if (currentStep < animSteps.size()) {
+                ofxAnimatableOfPoint newPhase = animSteps[currentStep];
+                //animSteps.erase(animSteps.begin());
+
+                //if (newPhase.getDuration() <= 0.0f) {
+                  //  anim = newPhase;
+               // }
+               // else {
+                 //   anim.setCurve(QUADRATIC_EASE_OUT); //get from newPhase if needed
+                  //  anim.setDuration(newPhase.getDuration());
+                   // anim.animateTo(newPhase.getCurrentPosition());
+                //}
+                anim = animSteps[currentStep];
+                currentStep++;
+            }
+            else {
+                playing = false;
+                EventArg arg = { this };
+                ofNotifyEvent(eventQueueDone, arg);
+            }
+        }
+    }
+}
+
+
+ofxAnimatableOfPoint ofxAnimatableQueue2552::getCurrentValue() {
+    return anim;
+}
+
+void ofxAnimatableQueue2552::startPlaying() {
+    currentStep = 0;
+    playing = true;
+}
+
+void ofxAnimatableQueue2552::pausePlayback() {
+    playing = false;
+}
+
+void ofxAnimatableQueue2552::resumePlayback() {
+    playing = true;
+}
 //
 //--------------------------------------------------------------
 void ofApp::setup(){
+    ofSetFrameRate(fps);
     ofBackground(ofColor::black);
     ofEnableLighting();
     ofEnableDepthTest();
@@ -61,11 +125,35 @@ void ofApp::setup(){
     light.setAmbientColor(ofColor::beige);
     light.setAreaLight(ofGetHeight() / 4, ofGetWidth() / 4);
     light.setPosition(-ofGetWidth() / 2, ofGetHeight(), 200);
+
+    eyeMotion.setDuration(1.3);
+    eyeMotion.setPosition(ofPoint(0, 0));
+    eyeMotion.setRepeatType(LOOP_BACK_AND_FORTH);
+    eyeMotion.setCurve(QUADRATIC_EASE_OUT);
+    eyeMotion.setRepeatTimes(300);
+    eyeMotion.setAutoFlipCurve(true);
+    eyeMotion.animateTo(ofPoint(10, 35));
+
+    //we want to know when the animation ends
+    ofAddListener(queue.eventQueueDone, this, &ofApp::onAnimQueueDone);
+    queue.addTransition(eyeMotion);
+    eyeMotion.animateTo(ofPoint(5, 45));
+    queue.addTransition(eyeMotion);
+    queue.startPlaying(); //start the animation
 }
 
 //--------------------------------------------------------------
 void ofApp::update(){
     eyes[0].update();
+
+    eyeMotion.update(1.0f / fps);
+    queue.update(1.0f / fps);
+
+    // debug helper
+    std::stringstream ss;
+    ss << queue.getCurrentValue().getCurrentPosition();
+    ofSetWindowTitle(ss.str());
+
 }
 
 //--------------------------------------------------------------
@@ -78,16 +166,21 @@ void ofApp::draw(){
     ofSetColor(ofColor::white);
     float size = std::min(ofGetHeight(), ofGetHeight());
     float r = size / 3;
-    ofRotateDeg(180, 1, 0, 0); // hide seam
+    ofPoint p = queue.getCurrentValue().getCurrentPosition();
+    //ofPoint p = eyeMotion.getCurrentPosition();
+    ofRotateXDeg(180); // hide seam
+    ofRotateXDeg(p.x);
+    ofRotateYDeg(p.y);
+    ofRotateZDeg(p.z);
     ofDrawSphere(0, 0, 0, r);
     unBindEye();
-    ofRotateDeg(180, 1, 0, 0); // hide seam
-    setPupil(r, 1);
+    setPupil(-r, 1);
     ofPopStyle();
     ofPopMatrix();
 }
+// allow for various eyes
 void ofApp::unBindEye() {
-    eyes[0].getTexture().unbind(); // allow for various eyes
+    eyes[0].getTexture().unbind();
 }
 void ofApp::bindEye() {
     eyes[0].getTexture().bind(); // allow for various eyes
@@ -116,6 +209,10 @@ void ofApp::setPupil(float r, int type) {
     }
 
 }
+void ofApp::onAnimQueueDone(ofxAnimatableQueue2552::EventArg&) {
+    queue.startPlaying(); //loop the animation endlessly
+}
+
 //--------------------------------------------------------------
 void ofApp::keyPressed(int key){
 
@@ -170,3 +267,4 @@ void ofApp::gotMessage(ofMessage msg){
 void ofApp::dragEvent(ofDragInfo dragInfo){ 
 
 }
+
