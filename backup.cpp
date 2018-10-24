cc4997e49fce25ca2d6c8e1468ac724bfd16c71a
Mark S <mark.shavlik@shavlik.us>
2018-10-24 11:25:20 -0500

    Update backup.cpp

--- backup.cpp
+++ backup.cpp
@@ -1,10 +1,199 @@
-#pragma once
+##pragma once
 
 #include "ofMain.h"
 #include "ofxAnimatableFloat.h"
 #include "ofxAnimatableOfPoint.h"
 #include "ofxAnimatableOfColor.h"
 #include "ofxAnimatableQueue.h"
+#include "ofxOpenCv.h"
+
+class MotionData {
+public:
+    MotionData() {}
+    MotionData(glm::vec2&r, ofRectangle &rec, int index = 0) {
+        rotation = r;
+        rect = rec;
+        this->index = index;
+    }
+    void draw(int scaleX, int scaleY, bool fill = false) const {
+        ofRectangle rectToScale = rect;
+        rectToScale.scale(scaleX, scaleY);
+        if (fill) {
+            ofFill();
+        }
+        else {
+            ofNoFill();
+        }
+        ofDrawRectangle(rectToScale.x*scaleX, rectToScale.y*scaleY, rectToScale.width, rectToScale.height);
+    }
+    bool found(int x, int y) {
+        return rect.inside(x, y);
+    }
+    glm::vec2 rotation;
+    ofRectangle rect; // target
+    int index = 0;
+};
+class Timer {
+public:
+    uint64_t t1 = 0;
+    uint64_t duration = 60; // 2 sec or so
+    void start() {
+        t1 = ofGetFrameNum();
+    }
+    bool current() {
+        return duration > ofGetFrameNum() - t1;
+    }
+};
+class myBlob : public ofxCvBlob {
+public:
+    void set(ofxCvBlob& b) {
+        blob = b;
+        t.duration = 5;
+        t.start();
+    }
+    Timer t;
+    ofxCvBlob blob;
+};
+class Contours {
+public:
+    std::vector<MotionData> motionMap;
+
+    const MotionData& find(int x, int y) {
+        for (auto& motion : motionMap) {
+            if (motion.found(x, y)) {
+                return motion;
+            }
+        }
+        return motionMap[0];// default to empty data, no movement needed
+    }
+    //v.x - motion x, v.y motion y, v.w - width, v.z - length
+    void add(const ofRectangle& grid, int index) { // private
+        float w = grid.getWidth();
+        float h = grid.getHeight();
+        float row = motionMap.size() / 4;
+        motionMap.push_back(MotionData(glm::vec2(grid.getX(), grid.getY()), ofRectangle(w * 0, row*h, w, h), index)); //ex: row 1, col 1
+        motionMap.push_back(MotionData(glm::vec2(grid.getX() + 5.0, grid.getY()), ofRectangle(w * 1, row*h, w, h), index + 1));// row 1, col 2
+        motionMap.push_back(MotionData(glm::vec2(grid.getX() + 7.0, grid.getY()), ofRectangle(w * 2, row*h, w, h), index + 2)), index + 2;// row 1, col 3
+        motionMap.push_back(MotionData(glm::vec2(grid.getX() + 9.0, grid.getY()), ofRectangle(w * 3, row*h, w, h), index + 3));// row 1, col 4
+    }
+    void setup() {
+        //video.setVerbose(true);
+        vector<ofVideoDevice> devices = video.listDevices();
+        for (auto& device : devices) {
+            if (device.deviceName.find("FaceTime") == std::string::npos) {
+                video.setDeviceID(device.id);
+                break;
+            }
+        }
+        float w = imgWidth / 4;
+        float h = imgHeight / 4;
+        // 0-360
+        add(ofRectangle(-12.0, 10.0, w, h), 0); // col 0, % of 180
+        add(ofRectangle(-10.0, 5.0, w, h), 4); // col 1
+        add(ofRectangle(10, -7.0, w, h), 8); // col 2
+        add(ofRectangle(12, -10.0, w, h), 12); // col 3
+
+        video.setVerbose(true);
+        video.setup(imgWidth, imgHeight);
+        colorImg.allocate(imgWidth, imgHeight);
+        grayImage.allocate(imgWidth, imgHeight);
+        grayDiff.allocate(imgWidth, imgHeight);
+
+    }
+    void update() {
+        video.update();
+        if (video.isFrameNew()) { // && (ofGetFrameNum() & 1) to slow things down
+                                  // clear less often
+            colorImg.setFromPixels(video.getPixels());
+            grayImage = colorImg; // convert our color image to a grayscale image
+                                  //grayImage.blurHeavily();
+            if (backgroundImage.bAllocated) {
+                grayDiff.absDiff(backgroundImage, grayImage);
+            }
+            backgroundImage = grayImage; // only track new items -- so eye moves when objects move
+            grayDiff.threshold(50); // turn any pixels above 30 white, and below 100 black
+            contourFinder.findContours(grayDiff, 5, (imgWidth*imgHeight), 128, false, true);
+        }
+    }
+    void draw(const ofxCvBlob& blob) {
+        ofPolyline line;
+        //video.draw(video.getWidth(),0,-video.getWidth(),video.getHeight());
+        for (int i = 0; i < blob.nPts; i++) {
+            line.addVertex(ofPoint(imgWidth - blob.pts[i].x, blob.pts[i].y));
+            //ofVertex(blob.pts[i].x, blob.pts[i].y);
+        }
+        line.close();
+        //line.scale(cx / imgWidth, cy / imgHeight);
+        line.draw();
+        ofSetHexColor(0xff0099);
+        //ofDrawRectangle(x + blob.boundingRect.x, y + blob.boundingRect.y, blob.boundingRect.width, blob.boundingRect.height);
+    }
+    void draw() {
+        ofPushStyle();
+        ofNoFill();
+        ofSetLineWidth(1);// ofRandom(1, 5));
+        bool first = true;
+        for (auto& blob : contourFinder.blobs) {
+            if (first) {
+                ofNoFill();
+                // for (auto& motion : motionMap) {
+                ///     motion.draw(cx / imgWidth, cy / imgHeight);
+                //}
+                MotionData data = find(imgWidth - blob.centroid.x, blob.centroid.y);
+                first = false; // largest blob is first
+            }
+            myBlob b;
+            b.set(blob);
+            myBlobs.push_back(b);
+        }
+        if (contourFinder.blobs.size() == 0) {
+        }
+        ofSetColor(ofColor(ofRandom(100, 255), ofRandom(100, 255), ofRandom(100, 255)));
+        for (auto blob = myBlobs.begin(); blob != myBlobs.end(); ) {
+            if ((*blob).t.current()) {
+                //light.setAmbientColor(ofColor(ofRandom(255), ofRandom(255), ofRandom(255)));
+                draw((*blob).blob);
+                ++blob;
+            }
+            else {
+                blob = myBlobs.erase(blob);
+            }
+        }
+
+        ofPopStyle();
+    }
+    ofxCvContourFinder contourFinder;
+    ofVideoGrabber video;
+    ofxCvColorImage colorImg;
+    ofxCvGrayscaleImage grayImage, backgroundImage, grayDiff;
+    std::vector<myBlob> myBlobs;
+    const int imgWidth = 320;// 320; // the motion image from the camera
+    const int imgHeight = 240;//240;
+};
+
+// get all logic into one place
+class ElectricCat {
+public:
+    void setup() {
+        ofSetColor(ofColor::white);
+        ofSetBackgroundColor(ofColor::black);
+        ofSetFrameRate(30);
+        countours.setup();
+        ofSetVerticalSync(true);
+        // Set the video grabber to the ofxPS3EyeGrabber.
+    }
+    void update() {
+        countours.update();
+    }
+    void draw() {
+        ofEnableDepthTest();
+        countours.draw();
+        //cam.end();
+        // put on the sun shades
+    }
+    Contours countours;
+
+};
 
 class ofxAnimatableQueueOfPoint {
 
@@ -219,6 +408,7 @@ class ofApp : public ofBaseApp{
 
 private:
 };
+
 
 #include "ofApp.h"
 
